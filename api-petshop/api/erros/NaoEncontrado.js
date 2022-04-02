fc76d31ccc348e1eb5f2b73bcfbc000cb00854b9
JoelmaWerlick <100633937+JoelmaWerlick@users.noreply.github.com>
2022-04-01 23:27:33 -0300

    Delete api-petshop/api directory

--- api-petshop/api/erros/NaoEncontrado.js
+++
@@ -1,9 +0,0 @@
-class NaoEncontrado extends Error {
-    constructor (nome) {
-        super(`${nome} não foi encontrado!`)
-        this.name = 'NaoEncontrado'
-        this.idErro = 0
-    }
-}
-
-module.exports = NaoEncontrado
